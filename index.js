292fbb0c22ab449cc0064fa295a9f36f93e182cf
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-16 21:16:03 +0300

    commit tree 6c7fe2b16f48818d99c16c8cd072fa0d617b4429

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+ //Add your code here
+const userInfo = {
+name: "Steve",
+email: "steve@steve.com",
+}
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(response => response.json())
+    .then(object => document.body.innerHTML=object['id'])
+    .catch(error => document.body.innerHTML = error.message)
+    
+    
+}
